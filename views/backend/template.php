61d22e2862b90f0453659c4d32bb5ce0a1b84d03
Sealk <Sealk91@lilo.org>
2020-04-27 12:48:48 +0200

    test dropdown

--- views/backend/template.php
+++ views/backend/template.php
@@ -3,9 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="./assets/css/header.template.css">
-    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> -->
-    <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"> -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
     <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script> -->
     <!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script> -->
     
@@ -27,9 +27,9 @@
             <li><a href="./index.php?action=list_cat">Catégories</a></li>
             <li><a href="#news">Produits</a></li>
             <li><a href="#news">News</a></li>
-            <li class="dropdown">
+            <li class="dropdown2">
               <a href="" class="dropbtn">Connexion</a>
-              <div class="dropdown-content">
+              <div class="dropdown2-content">
                 <a href="#">Link 1</a>
                 <a href="#">Link 2</a>
                 <a href="#">Link 3</a>
